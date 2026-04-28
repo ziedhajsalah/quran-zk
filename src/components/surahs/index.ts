f7c7944eb5bacc473ac9ee2b33d4f5ee10ea1dbc
Zied Ben Haj Salah <ziedhajsalah@gmail.com>
2026-04-28 23:15:27 +0100

    Re-export review-assignment components from surahs barrel

---
+++ src/components/surahs/index.ts
@@ -0,0 +1,17 @@
+export { SurahGradeList } from './SurahGradeList'
+export { AddSurahDrawer } from './AddSurahDrawer'
+export type { AddSurahDrawerProps } from './AddSurahDrawer'
+export { SurahGradeRow } from './SurahGradeRow'
+export { StudentsPicker } from './StudentsPicker'
+export type { StudentSummary, StudentsPickerProps } from './StudentsPicker'
+export { SurahReviewQueue } from './SurahReviewQueue'
+export type {
+  SurahReviewQueueItem,
+  SurahReviewQueueProps,
+} from './SurahReviewQueue'
+export { SurahReviewRow } from './SurahReviewRow'
+export type { SurahReviewRowProps } from './SurahReviewRow'
+export { AssignReviewDrawer } from './AssignReviewDrawer'
+export type { AssignReviewDrawerProps } from './AssignReviewDrawer'
+export { CloseAssignmentDialog } from './CloseAssignmentDialog'
+export type { CloseAssignmentDialogProps } from './CloseAssignmentDialog'
